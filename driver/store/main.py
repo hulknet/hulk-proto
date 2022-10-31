4f6ad39334f5b7f2c16cc5878edc5320e3ed4dc2
kotfalya <kotfalya@gmail.com>
2022-10-31 22:19:15 +0100

    store: init

--- driver/store/main.py
+++ driver/store/main.py
@@ -1,9 +1,36 @@
 import asyncio
 
+from lib.types import ID8
+
+store = dict[ID8, bytes]
+
 
 async def handle(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    print(await reader.readexactly(1))
+    command_id = await reader.readexactly(1)
+    if command_id == b'\x00':
+        await handle_get(reader, writer)
+    if command_id == b'\x01':
+        await handle_set(reader, writer)
     pass
+
+
+async def handle_get(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    key_len = await reader.readexactly(1)
+    key = await reader.readexactly(key_len)
+    value = await reader.readexactly(1)
+    writer.write(value)
+    await writer.drain()
+    writer.close()
+
+
+async def handle_set(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    key_len = await reader.readexactly(1)
+    key = await reader.readexactly(key_len)
+    value_len = await reader.readexactly(1)
+    value = await reader.readexactly(value_len)
+    writer.write(b'\x00')
+    await writer.drain()
+    writer.close()
 
 
 async def main():
